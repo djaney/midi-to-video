65001918408ff980ca62aa39419557082dd8181c
Djane Rey Mabelin <d.mabelin@arcanys.com>
2019-03-14 22:02:03 +0800

    Create video

--- music.py
+++ music.py
@@ -4,6 +4,7 @@ import midi
 from midi import constants
 import math
 import os
+from moviepy.editor import CompositeVideoClip, VideoFileClip
 
 
 def value_to_note(value):
@@ -84,7 +85,7 @@ def calculate_seconds_per_tick(tempo, resolution):
     return 60 * 1000000 / tempo / resolution / 1000000
 
 
-def generate_track_plan(event_map, video_map, resolution, max_ticks, initial_tempo):
+def generate_track_plan(event_map, resolution, max_ticks, initial_tempo):
     plan = []
     notes_currently_playing = {}
     currently_playing_timeline = []
@@ -95,15 +96,12 @@ def generate_track_plan(event_map, video_map, resolution, max_ticks, initial_tem
             events = event_map[current_tick]
             max_stack = max(len(events), max_stack)
             for event in events:
-                if type(event) is midi.TrackNameEvent:
-                    track_name = event.text
-                    print(track_name)
-                elif type(event) is midi.SetTempoEvent:
+                if type(event) is midi.SetTempoEvent:
                     seconds_per_tick = calculate_seconds_per_tick(event.get_bpm(), resolution)
                 elif type(event) is midi.NoteOnEvent:
                     note, octave = value_to_note(event.get_pitch())
                     turn_on_note(note, octave, event.channel, seconds_per_tick, current_tick, plan,
-                                 notes_currently_playing)
+                                 notes_currently_playing, event.get_velocity())
                 elif type(event) is midi.NoteOffEvent:
                     note, octave = value_to_note(event.get_pitch())
                     turn_off_note(note, octave, event.channel, seconds_per_tick, current_tick, plan,
@@ -111,32 +109,24 @@ def generate_track_plan(event_map, video_map, resolution, max_ticks, initial_tem
 
         currently_playing_timeline.append(list(notes_currently_playing.keys()))
 
-    for p in currently_playing_timeline:
-        print(p)
-    for p in plan:
-        current_tick, seconds_in_track, note, octave, duration = p
-        if duration == 0:
-            duration = "sustain"
-            print("{} [{:.4f}] Play {}{} {}".format(current_tick, seconds_in_track, note, octave, duration))
-        else:
-            print("{} [{:.4f}] Play {}{} for {:.4f} seconds".format(current_tick, seconds_in_track, note, octave, duration))
+    return plan, currently_playing_timeline
 
 
 def get_note_key(note, octave, channel):
     return "{}{}-{}".format(note, octave, channel)
 
 
-def turn_on_note(note, octave, channel, seconds_per_tick, current_tick, plan, notes_currently_playing):
+def turn_on_note(note, octave, channel, seconds_per_tick, current_tick, plan, notes_currently_playing, velocity):
     seconds_in_track = seconds_per_tick * current_tick
     key = get_note_key(note, octave, channel)
 
     # turn off not if already playing
     if key in notes_currently_playing:
         note_index = notes_currently_playing[key]
-        t, s, n, o, d = plan[note_index]
+        t, s, n, o, d, v = plan[note_index]
         turn_off_note(n, o, channel, seconds_per_tick, current_tick, plan, notes_currently_playing)
 
-    plan.append((current_tick, seconds_in_track, note, octave, 0))
+    plan.append((current_tick, seconds_in_track, note, octave, 0, velocity))
     notes_currently_playing[key] = len(plan) - 1
 
 
@@ -145,9 +135,9 @@ def turn_off_note(note, octave, channel, seconds_per_tick, current_tick, plan, n
     current_seconds_in_track = seconds_per_tick * current_tick
     if key in notes_currently_playing:
         note_index = notes_currently_playing[key]
-        current_tick, seconds_in_track, note, octave, duration = plan[note_index]
+        current_tick, seconds_in_track, note, octave, duration, velocity = plan[note_index]
         duration = current_seconds_in_track - seconds_in_track
-        plan[note_index] = (current_tick, seconds_in_track, note, octave, duration)
+        plan[note_index] = (current_tick, seconds_in_track, note, octave, duration, velocity)
         notes_currently_playing.pop(key)
 
 
@@ -167,6 +157,41 @@ def map_videos(video_dir):
         video_map[note_name] = path
     return video_map
 
+
+def create_video(size, plan, timeline, video_map, notification_callback=None, end=None, start=None):
+    clips = []
+    if end is not None and end < len(plan):
+        end_time = end
+    else:
+        # time of last frame
+        end_time = plan[-1][1]
+    for current_tick, seconds_in_track, note, octave, duration, velocity in plan:
+
+        if start is not None and seconds_in_track < start:
+            continue
+
+        video_key = "{}{}".format(note, octave)
+        if video_key in video_map:
+            # get a fragment of the clip
+            if start is not None:
+                clip_start = seconds_in_track - start
+            else:
+                clip_start = seconds_in_track
+            if clip_start < 0:
+                clip_start = 0
+            clip = VideoFileClip(video_map[video_key]).subclip(0, duration).set_start(clip_start)
+            clips.append(clip)
+            notification_callback(seconds_in_track, end_time)
+            if end is not None and end_time <= seconds_in_track:
+                break
+
+    return CompositeVideoClip(clips, size=size)
+
+
+def loader(current, total):
+    print("{}/{}".format(current, total))
+
+
 def main(args):
     pattern = midi.read_midifile(args.midi_path)
     video_map = map_videos(args.video_dir)
@@ -174,15 +199,22 @@ def main(args):
         print(get_track_names(pattern))
     else:
 
-        max_ticks, resolution, initial_tempo = analyze_midi(pattern)
+        total_ticks, resolution, initial_tempo = analyze_midi(pattern)
         track = grab_track_by_name(pattern, args.track)
         event_map = map_events_by_tick(track)
-        generate_track_plan(event_map, video_map, resolution, max_ticks, initial_tempo)
+        plan, timeline = generate_track_plan(event_map, resolution, total_ticks, initial_tempo)
+        video = create_video((360, 240), plan, timeline, video_map, notification_callback=loader,
+                             end=args.end, start=args.start)
+        # save
+        video.write_videofile(args.output)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('midi_path', type=str)
-    parser.add_argument('--track', type=str, default='')
-    parser.add_argument('--video_dir', type=str, default='videos')
+    parser.add_argument('--track', '-t', type=str, default='')
+    parser.add_argument('--video_dir', '-d', type=str, default='videos')
+    parser.add_argument('--output', '-o', type=str, default='output.mp4')
+    parser.add_argument('--start', '-s', type=int, default=None)
+    parser.add_argument('--end', '-e', type=int, default=None)
     main(parser.parse_args())
